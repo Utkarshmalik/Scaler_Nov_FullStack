652280ac3c8f0e2ec3b06e4ed30fc8a7f53c3fcb
utmalik <utmalik@amazon.com>
2024-02-05 08:54:43 +0530

    React Lecture 1

---
+++ React/ecommerce/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import ProductList from './Components/ProductList/ProductList';
+
+
+function App(){
+
+  return <div>
+
+    <h1> Ecommerce Application </h1>
+
+
+    <ProductList/>
+
+
+  </div>
+
+
+}
+
+
+export default App;
---
+++ React/ecommerce/src/Components/Product/Product.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+function Product(props){
+
+    const name  = props.data.title;
+
+
+    return <h3> Product :  {name}  </h3>
+
+}
+
+
+export default Product;
